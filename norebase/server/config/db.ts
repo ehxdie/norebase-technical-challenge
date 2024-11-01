ea66a90e4e265f2ab4310b43a8ae70104977ab48
Edidiong <ephraimedy@gmail.com>
2024-11-01 19:18:15 +0100

    Improved db.ts

--- norebase/server/config/db.ts
+++ norebase/server/config/db.ts
@@ -1,12 +1,31 @@
 import mongoose from 'mongoose';
 
+const MONGO_URI = process.env.MONGO_URI;
+
+if (!MONGO_URI) {
+    throw new Error("Missing MONGO_URI in environment variables");
+}
+
+let isConnected: boolean = false; // track the connection status
+
 const connectDB = async (): Promise<void> => {
+    if (isConnected) {
+        console.log("Using existing MongoDB connection");
+        return;
+    }
+
     try {
-        await mongoose.connect(process.env.MONGO_URI || '');
-        console.log('MongoDB connected');
+        const connection = await mongoose.connect(MONGO_URI);
+
+        isConnected = connection.connections[0].readyState === 1;
+        if (isConnected) {
+            console.log("MongoDB connected");
+        } else {
+            console.log("Failed to connect to MongoDB");
+        }
     } catch (error) {
-        console.error('Database connection error:', error);
-        process.exit(1);
+        console.error("MongoDB connection error:", error);
+        throw new Error("Failed to connect to MongoDB");
     }
 };
 
